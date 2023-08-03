01565a930863dd61aed86399c1334656707b179f
Pelin BİNGÖL <126492207+pelinbingl@users.noreply.github.com>
2023-08-03 11:42:59 +0300

    Add files via upload

---
+++ Insertion_sort/Insertion_sort/Insertion_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = { 64, 25, 12, 22, 11 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << "Array:";
+    for (int i = 0; i < size; i++)
+    {
+        cout<< arr[i] <<" ";
+    }
+    cout << endl;
+    insertionSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
